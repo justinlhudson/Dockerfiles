72f6044a4f79977b6612976a9ce2aa9717617806
justinlhudson <justinlhudson@gmail.com>
2017-03-06 09:17:05 -0500

    better stopping

--- Torified/startup.sh
+++ Torified/startup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+_image=$1
+
 _here="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo $_here
 cd $_here
@@ -8,12 +10,13 @@ docker-compose stop
 
 # HACK: internet not working for docker?
 sudo service docker restart
-sleep 5
 sudo iptables -t nat -F
 sudo ifconfig docker0 down
 sudo brctl delbr docker0
 sudo service docker restart
-sleep 5
+sleep 1
+docker stop $(docker ps -a -q --filter="name=$_image")
+sleep 1
 
 docker-compose create
 docker-compose start
